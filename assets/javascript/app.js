f302213625ed2e0dcaefa8fbe8ee7bcece160c8e
Llenrep <lppernell1998@gmai.com>
2018-11-18 14:47:30 -0500

    added index, js, and style, used music artists, got own apikey

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+$("button").on("click", function(){
+    var person = $(this).attr("name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    person + "&api_key=GQ6BlBv7dj9fF8q1vHfseubUDIljPR8a&limit=10";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+        var results = response.data; 
+
+        for (var i = 0; i < results.length; i++){ 
+
+            var gifDiv = $("<div>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var personImage = $("<img>");
+
+            personImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+
+            gifDiv.prepend(personImage);
+
+            $("#gifsGoHere").prepend(gifDiv);        
+        }
+    
+    })  
+})
